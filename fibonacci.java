5f8d2b8f4f3eac6d2c6193ce5dbf833652c3efeb
lipi376 <lipivats3@gmail.com>
2025-08-14 11:33:27 +0530

    fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FibonacciNonRecursive {
+    public static void printFibonacci(int n) {
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int terms = scanner.nextInt();
+        printFibonacci(terms);
+
+        scanner.close();
+    }
+}
