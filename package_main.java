f55ceb4eefc343e2dd71c252b1219f6518112a47
lipi376 <lipivats3@gmail.com>
2025-11-06 20:39:12 +0530

    Create package_main.java

---
+++ package_main.java
@@ -0,0 +1,8 @@
+import mypackage.MyClass;
+  
+ public class PackageDemo {
+  public static void main(String[] args) {
+  MyClass obj = new MyClass();
+  System.out.println("Value from package: " + obj.x);
+  }
+ }
