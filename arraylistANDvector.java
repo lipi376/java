df71b6c3beb0a89b9672e542522b8ca6d9c90b9f
lipi376 <lipivats3@gmail.com>
2025-11-06 20:46:34 +0530

    Create arraylistANDvector.java

---
+++ arraylistANDvector.java
@@ -0,0 +1,19 @@
+import java.util.*;
+  
+ public class CollectionExample {
+  public static void main(String[] args) {
+
+  ArrayList<String> list = new ArrayList<>();
+  list.add("Java");
+  list.add("Python");
+  list.add("C++");
+  System.out.println("ArrayList: " + list);
+  
+
+  Vector<Integer> vector = new Vector<>();
+  vector.add(10);
+  vector.add(20);
+  vector.add(30);
+  System.out.println("Vector: "+ vector);
+  }
+ }
